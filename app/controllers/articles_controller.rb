3c1bf6ebc3c8ff859bddee3ef444d713a9f5eeea
Manon Pilaud <mandyypilaud@gmail.com>
2018-12-15 15:11:01 -0500

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,8 +25,8 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
-@article.update(title: params[:article][:title], description: params[:article][:description])
-redirect_to article_path(@article)
+    @article.update(title: params[:article][:title], description: params[:article][:description])
+    redirect_to article_path(@article)
   end
   # add edit and update methods here
 end
